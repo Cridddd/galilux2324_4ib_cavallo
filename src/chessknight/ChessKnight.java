d36c24d358d13d1ba8d684fd4080c69a84a13619
Mad-Mind <0luke.2.2.1n0@gmail.com>
2024-04-16 19:00:10 +0200

    Aggiunta di return su ogni mossa anche quelle non valide, aggiunta di fine programma a scelta dell'utente.

---
+++ src/chessknight/ChessKnight.java
@@ -0,0 +1,27 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Main.java to edit this template
+ */
+package chessknight;
+
+/**
+ *
+ * @author matte
+ */
+public class ChessKnight {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+    int n = 1;
+    char l ='b';
+    
+    KnightMove m = new KnightMove();
+    m.mosse(n,l); //richiamo il metodo
+    }
+    
+}
+
+
+//prova per vedere se il commit funziona
