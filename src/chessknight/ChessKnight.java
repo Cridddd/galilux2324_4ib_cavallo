6a29e6f153e5a50e39d35f730acfcf4048ff2556
Mad-Mind <0luke.2.2.1n0@gmail.com>
2024-04-23 10:13:15 +0200

    Indentazione fatta

--- src/chessknight/ChessKnight.java
+++ src/chessknight/ChessKnight.java
@@ -6,22 +6,20 @@ package chessknight;
 
 /**
  *
- * @author matte
+ * @author matteo e marco
  */
-public class ChessKnight {
+public class ChessKnight 
+{
 
     /**
      * @param args the command line arguments
      */
-    public static void main(String[] args) {
-    int n = 1;
-    char l ='b';
-    
-    KnightMove m = new KnightMove();
-    m.mosse(n,l); //richiamo il metodo
+    public static void main(String[] args) 
+    {
+        int n = 1;
+        char l ='b';
+        KnightMove m = new KnightMove();
+        m.mosse(n,l); //richiamo il metodo
     }
     
 }
-
-
-//prova per vedere se il commit funziona
